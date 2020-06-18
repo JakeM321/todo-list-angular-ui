47bacac5515e36bddcea3dd8522ec86f7a75bad7
Jake Marshall <jake.marshall123@outlook.com>
2020-06-18 15:11:45 +1000

    Started building login UI

--- src/modules/login/components/login-ui/login-ui.component.ts
+++ src/modules/login/components/login-ui/login-ui.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-login-ui',
   templateUrl: './login-ui.component.html',
-  styleUrls: ['./login-ui.component.less']
+  styleUrls: ['./login-ui.component.less'],
+  encapsulation: ViewEncapsulation.None
 })
 export class LoginUiComponent implements OnInit {
 
